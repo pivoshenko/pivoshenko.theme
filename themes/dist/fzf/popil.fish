d15d6a5e5e38ceae47eb35c104c10aed1965bf84
pivoshenko <pivoshenko@users.noreply.github.com>
2026-05-25 21:42:37 +0100

    revert: restore themes/dist tracked (both flavors), keep committed per convention

---
+++ themes/dist/fzf/popil.fish
@@ -0,0 +1,6 @@
+set -Ux FZF_POPIL "\
+--color=bg+:#262625,bg:#1f1f1e,spinner:#e2cfc6,hl:#c98787 \
+--color=fg:#e4e2de,header:#c98787,info:#a78cc4,pointer:#e2cfc6 \
+--color=marker:#9faece,fg+:#e4e2de,prompt:#a78cc4,hl+:#c98787 \
+--color=selected-bg:#2e2e2c \
+--color=border:#57534e,label:#e4e2de"
