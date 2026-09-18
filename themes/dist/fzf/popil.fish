8fd4bee11ed736ed55fe68e340fbaf3b8a40a442
pivoshenko <pivoshenko@users.noreply.github.com>
2026-09-18 17:23:17 +0100

    feat(palettes): standardize the accent role map across flavors

--- themes/dist/fzf/popil.fish
+++ themes/dist/fzf/popil.fish
@@ -1,6 +1,6 @@
 set -Ux FZF_POPIL "\
---color=bg+:#262625,bg:#1f1f1e,spinner:#ddc7c2,hl:#d97757 \
---color=fg:#e4e2de,header:#d97757,info:#b88aa0,pointer:#ddc7c2 \
---color=marker:#a89bb5,fg+:#e4e2de,prompt:#b88aa0,hl+:#d97757 \
+--color=bg+:#262625,bg:#1f1f1e,spinner:#ddc7c2,hl:#7ba0c4 \
+--color=fg:#e4e2de,header:#7ba0c4,info:#b88aa0,pointer:#ddc7c2 \
+--color=marker:#a89bb5,fg+:#e4e2de,prompt:#b88aa0,hl+:#7ba0c4 \
 --color=selected-bg:#2e2e2c \
 --color=border:#57534e,label:#e4e2de"
--- themes/dist/fzf/vatra.fish
+++ themes/dist/fzf/vatra.fish
@@ -1,6 +1,6 @@
 set -Ux FZF_VATRA "\
---color=bg+:#262625,bg:#1f1f1e,spinner:#edbdb4,hl:#ed8438 \
---color=fg:#e4e2de,header:#ed8438,info:#ae91d1,pointer:#edbdb4 \
---color=marker:#919add,fg+:#e4e2de,prompt:#ae91d1,hl+:#ed8438 \
+--color=bg+:#262625,bg:#1f1f1e,spinner:#edbdb4,hl:#91a6c8 \
+--color=fg:#e4e2de,header:#91a6c8,info:#ae91d1,pointer:#edbdb4 \
+--color=marker:#919add,fg+:#e4e2de,prompt:#ae91d1,hl+:#91a6c8 \
 --color=selected-bg:#2e2e2c \
 --color=border:#57534e,label:#e4e2de"
