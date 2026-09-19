4ee6062d917a3459b6e849161cf7a4a9673ffd57
pivoshenko <pivoshenko@users.noreply.github.com>
2026-09-19 10:40:29 +0100

    fix(palettes): match the popil surfaces to the base warmth

--- themes/dist/fzf/popil.fish
+++ themes/dist/fzf/popil.fish
@@ -1,6 +1,6 @@
 set -Ux FZF_POPIL "\
---color=bg+:#292929,bg:#1f1f1e,spinner:#ddc7c2,hl:#8da7d1 \
+--color=bg+:#292928,bg:#1f1f1e,spinner:#ddc7c2,hl:#8da7d1 \
 --color=fg:#e4e2de,header:#8da7d1,info:#ad8cc0,pointer:#ddc7c2 \
 --color=marker:#b8b9d7,fg+:#e4e2de,prompt:#ad8cc0,hl+:#8da7d1 \
---color=selected-bg:#323232 \
+--color=selected-bg:#323231 \
 --color=border:#57534e,label:#e4e2de"
